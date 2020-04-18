e2cab28fefae624db1da408e62bf8158bee79899
康聪 <kangcong123321@163.com>
2020-04-18 10:06:46 +0800

    初始化

---
+++ src/assert1/AssertTest.java
@@ -0,0 +1,31 @@
+package assert1;
+
+/**
+ * @Describe:
+ * @Author: kangcong
+ * @Date: 2019/4/22 2:52 PM
+ * @Version 1.0
+ */
+public class AssertTest {
+    public static void main(String[] args) {
+//        String str = "111";
+//        assert str == null : "异常";
+//        System.out.println("111");
+
+        System.out.println(md2(11, 22, 33, 44));
+    }
+
+
+    static String md1(String... strings){
+        System.out.println(strings);
+        String s=strings[strings.length/2];
+        return s;
+    }
+
+    static <T> T md2(T... ts){
+        for (T t : ts) {
+            System.out.println(t);
+        }
+        return ts[ts.length/2];
+    }
+ }
